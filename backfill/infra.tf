5fde60d981d17502691ae28184c64d4c4b88ce77
syuhas <syuhas22@gmail.com>
2024-11-18 03:09:36 -0500

    timeout 900

--- backfill/infra.tf
+++ backfill/infra.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "backfill" {
   role          = "arn:aws:iam::551796573889:role/lambdaAdmin"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
-  timeout       = 1800
+  timeout       = 900
   memory_size   = 256
   source_code_hash = filebase64sha256("lambda_function.zip")
 }
